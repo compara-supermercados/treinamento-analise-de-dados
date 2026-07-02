57db92f5698a5d27b7bfa9c2b1cb46d3616e64d9
compara-supermercados <cassianoroc@gmail.com>
2026-07-02 16:40:34 -0300

    feat: adiciona notebook da aula 03

---
+++ aula01.py
@@ -0,0 +1,17 @@
+#O comando print exibe uma informação na tela do usuário.
+print("Olá mundo!")
+print("Inicinando treinamento de Análise de Dados")
+print(30) #Idade do usuário
+print("30") #Idade do usuário
+
+idade = 30 #Idade 1
+idade_dois = "30" #Idade2
+
+print(idade)
+print(idade_dois)
+
+idade_futura = idade + 1
+print(idade_futura)
+
+print(type(idade))
+print(type(idade_dois))
